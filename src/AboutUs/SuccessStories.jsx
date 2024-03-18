e7017cdf8ed11cc7173696adb45e7e1860c5532a
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-03-18 21:05:44 +0530

    .

--- src/AboutUs/SuccessStories.jsx
+++ src/AboutUs/SuccessStories.jsx
@@ -1,56 +1,64 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/free-mode";
-
-import { FreeMode, Pagination } from "swiper/modules";
-
-import { RxArrowTopRight } from "react-icons/rx";
-import { ServiceData } from "../Constants";
-
-const SuccessStories = () => {
+import React from "react";
+import { TECarousel, TECarouselItem } from "tw-elements-react";
+function SuccessStories() {
     return (
-        <div className="flex items-center justify-center flex-col h-[900px] bg-[#6c34af]">
-            <Swiper
-                breakpoints={{
-                    340: {
-                        slidesPerView: 2,
-                        spaceBetween: 15,
-                    },
-                    700: {
-                        slidesPerView: 3,
-                        spaceBetween: 15,
-                    },
-                }}
-                freeMode={true}
-                pagination={{
-                    clickable: true,
-                    arrow:true  
-                }}
-                modules={[FreeMode, Pagination]}
-                className="max-w-[90%] lg:max-w-[80%]"
-            >
-                {ServiceData.map((item) => (
-                    <SwiperSlide key={item.title}>
-                        <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
-                            <div
-                                className="absolute inset-0 bg-cover bg-center"
-                                style={{ backgroundImage: `url(${item.backgroundImage})` }}
-                            />
-                            <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
-                            <div className="relative flex flex-col gap-3">
-                                <item.icon className="text-blue-600 group-hover:text-blue-400 w-[32px] h-[32px]" />
-                                <h1 className="text-xl lg:text-2xl">{item.title} </h1>
-                                <p className="lg:text-[18px]">{item.content} </p>
-                            </div>
-                            <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    );
-};
+        <>
+        <div className="flex justify-center items-center mx-auto w-96 h-96 border border-red-500 ">
 
+            <TECarousel showControls showIndicators ride="carousel">
+                <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+                    <TECarouselItem
+                        itemID={1}
+                        className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                        >
+                        <img 
+                            src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(15).jpg"
+                            className="block w-full size-80"
+                            alt="..."
+                            />
+                        <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
+                            <h5 className="text-xl">First slide label</h5>
+                            <p>
+                                Some representative placeholder content for the first slide.
+                            </p>
+                        </div>
+                    </TECarouselItem>
+                    <TECarouselItem
+                        itemID={2}
+                        className="relative float-left hidden -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                        >
+                        <img
+                            src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(22).jpg"
+                            className="block w-full size-80"
+                            alt="..."
+                            />
+                        <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
+                            <h5 className="text-xl">Second slide label</h5>
+                            <p>
+                                Some representative placeholder content for the second slide.
+                            </p>
+                        </div>
+                    </TECarouselItem>
+                    <TECarouselItem
+                        itemID={3}
+                        className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                        >
+                        <img
+                            src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(23).jpg"
+                            className="block w-full size-80"
+                            alt="..."
+                            />
+                        <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
+                            <h5 className="text-xl">Third slide label</h5>
+                            <p>
+                                Some representative placeholder content for the third slide.
+                            </p>
+                        </div>
+                    </TECarouselItem>
+                </div>
+            </TECarousel>
+                            </div>
+        </>
+    );
+}
 export default SuccessStories;
