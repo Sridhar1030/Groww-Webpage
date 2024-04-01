2b9ca8ac8eaaca77c659522f3c787cdd6ac998b2
karan-2906 <karangandhi486@gmail.com>
2024-04-01 21:16:14 +0530

    edited

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -8,10 +8,10 @@ function AboutUs() {
                 <h1 className='font-semibold text-center text-4xl'>Know About
                     <span className="text-blue-500">"Grow Educators"</span>
                 </h1>
-                <div className='flex flex-col-reverse items-center justify-between lg:flex-row'>
+                <div className='flex flex-col-reverse items-center justify-center lg:gap-20 p-10 lg:flex-row'>
                     <div>
-                        <h2 className='text-3xl border'>Hey there ....
-                            <span className='mt-3 mb-10 justify-end'>I'm Saofin Yadav,
+                        <h2 className='text-3xl mt-4'>Hey there ....
+                            <span className='mt-3 mb-10 justify-end'>I'm Sachin Yadav,
                                 <p> Founder of "Grow Educators".</p>
                             </span>
                         </h2>
@@ -21,7 +21,7 @@ function AboutUs() {
                         <p className='mt-1 border border-red-500'>BE (Comps ENG)</p>
                     </div>
                 </div>
-                <p className=" text-black">
+                <p className=" text-black text-xl mb-5 lg:mx-14 font-serif">
                     At "Grow Educators", we focus on building a strong foundation of knowledge and concept in students for their success, providing an excellent platform for the preparation of competitive exams and board-level education.
                     We provide the best academic support and personal care which helps them meet their career goals and objectives.
 
