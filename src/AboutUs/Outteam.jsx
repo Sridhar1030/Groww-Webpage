1a7ace470f26e4821af70eda27a3c20848182585
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-04-04 14:07:37 +0530

    final changes done effects left

--- src/AboutUs/Outteam.jsx
+++ src/AboutUs/Outteam.jsx
@@ -10,61 +10,60 @@ const skills = [
     {
         id: 1,
         name: 'Mr Sachin Yadav',
-        qualification: 'BE computer science',
-        experience: '7 years',
+        qualification: '(BE computer science)',
+        experience: 'Teaching Experience 7 years',
         imgs: sachinImg,
     },
     {
         id: 2,
         name: 'Mr Akshesh Yadav',
-        qualification: 'M.B.B.S',
-        experience: '6 years',
+        qualification: '(M.B.B.S)',
+        experience: 'Teaching Experience 6 years',
         imgs: aksheshImg,
     },
     {
         id: 3,
         name: 'Mr Shubham Jaiswar',
-        qualification: 'M.B.B.S',
-        experience: '7 years',
+        qualification: '(M.B.B.S)',
+        experience: 'Teaching Experience 7 years',
         imgs: shubhamImg,
     },
     {
         id: 4,
         name: 'Mr Aakash Chaurasiya',
-        qualification: 'BE computer science',
-        experience: '7 years',
+        qualification: '(BE computer science)',
+        experience: 'Teaching Experience 7 years',
         imgs: aakashImg,
     },
     {
         id: 5,
         name: 'Mr Ankit Vishwakarma',
-        qualification: 'BE computer science',
-        experience: '6 years',
+        qualification: '(BE computer science)',
+        experience: 'Teaching Experience 6 years',
         imgs: ankitImg,
     },
     {
         id: 6,
         name: 'Mr Abhishek Chaubey',
-        qualification: 'B.Sc. Bed (Mathematics) ',
-        experience: '6 years',
+        qualification: 'B.Sc. Bed(Mathematics) ',
+        experience: 'Teaching Experience 6 years',
         imgs: abhishekImg,
     },
 ];
 
 const Outteam = () => {
     return (
-        <div className='rounded-lg flex justify-center m-5'>
-            <div className="container rounded-3xl slider-contain flex overflow-hidden" >
+        <div className='rounded-lg flex justify-center items-center'>
+            <div className="container slider-contain flex overflow-hidden" >
                 <div className=" slider flex overflow-visible">
                     {skills.map((skills, id) => {
                         return (
-                            <div className='flex flex-col justify-center items-center'>
-                                <img src={skills.imgs} alt="img" className='h-32 w-32 border-2 border-black flex flex-col z-50 rounded-full' />
-                                <div className=" items-center text-center  rounded-3xl -mt-10 h-44 pt-12 z-0 mx-3 hover:text-accent border-2 bg-slate-300 flex flex-col">
-                                    <h5 className=' w-52 mx-4 text-lg font-bold'>{skills.name}</h5>
-                                    <h5 className=' w-48 mx-4'>{skills.qualification}</h5>
-                                    <h5 className=' w-40 mx-4 mt-2'>Teaching Experience {skills.experience} </h5>
-                                </div>
+                            <div className=" items-center text-center rounded-3xl my-3 mx-3 hover:text-accent bg-slate-300 flex flex-col">
+                                <img src={skills.imgs} alt="img" className='h-32 w-32 flex flex-col  rounded-full' />
+                                <h5 className=' w-40 mx-4 font-semibold'>{skills.name}</h5>
+                                <h5 className=' w-40 mx-4'>{skills.qualification}</h5>
+                                <h5 className=' w-40 mx-4'>{skills.experience}</h5>
+
                             </div>
                         )
                     })}
@@ -72,28 +71,13 @@ const Outteam = () => {
                 <div className=" slider flex overflow-visible">
                     {skills.map((skills, id) => {
                         return (
-                            <div className='flex flex-col justify-center items-center'>
-                                <img src={skills.imgs} alt="img" className='h-32 w-32 border-2 border-black flex flex-col z-50 rounded-full' />
-                                <div className=" items-center text-center  rounded-3xl -mt-10 h-44 pt-12 z-0 mx-3 hover:text-accent border-2 bg-slate-300 flex flex-col">
-                                    <h5 className=' w-52 mx-4 text-lg font-bold'>{skills.name}</h5>
-                                    <h5 className=' w-48 mx-4'>{skills.qualification}</h5>
-                                    <h5 className=' w-40 mx-4 4 mt-2'>Teaching Experience {skills.experience}</h5>
-                                </div>
+                            <div className=" items-center text-center rounded-3xl my-3 mx-3 hover:text-accent bg-slate-300 flex flex-col">
+                                <img src={skills.imgs} alt="" className='w-32 h-32 item rounded-full' />
+                                <h5 className=' w-40 mx-4 font-semibold'>{skills.name}</h5>
+                                <h5 className=' w-40 mx-4'>{skills.qualification}</h5>
+                                <h5 className=' w-40 mx-4'>{skills.experience}</h5>
+
                             </div>
-                        )
-                    })}
-                </div>
-                <div className=" slider flex overflow-visible">
-                    {skills.map((skills, id) => {
-                        return (
-                            <div className='flex flex-col justify-center items-center'>
-                            <img src={skills.imgs} alt="img" className='h-32 w-32 border-2 border-black flex flex-col z-50 rounded-full' />
-                            <div className=" items-center text-center  rounded-3xl -mt-10 h-44 pt-12 z-0 mx-3 hover:text-accent border-2 bg-slate-300 flex flex-col">
-                                <h5 className=' w-52 mx-4 text-lg font-semibold'>{skills.name}</h5>
-                                <h5 className=' w-48 mx-4'>{skills.qualification}</h5>
-                                <h5 className=' w-40 mx-4 4 mt-2'>Teaching Experience {skills.experience}</h5>
-                            </div>
-                        </div>
                         )
                     })}
                 </div>
